8c26610a8979ca40b026ce06c022002e4bda4566
lamozavrik <lamozavrik@gmail.com>
2015-10-10 22:28:28 +0300

    окончательный вид адаптивного меню

--- cms/templates/admin/main.tpl.php
+++ cms/templates/admin/main.tpl.php
@@ -4,11 +4,49 @@
         <title><?=_('Панель управления')?></title>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
         <?php foreach(\core\View::styles() as $style): ?>
-        <link rel="stylesheet" type="text/css" href="/cms/templates/admin/css/<?=$style ?>" />
+        <link rel="stylesheet" type="text/css" href="<?=url('cms/templates/admin/css') . '/' . $style ?>" />
         <?php endforeach; ?>
+        <script type="text/javascript" src="<?=url('cms/media/js/jquery-1.11.3.min.js')?>"></script>
         <?php foreach(\core\View::javascript() as $js): ?>
-        <script type="text/javascript" src="/cms/templates/admin/js/<?=$js ?>"></script>
+        <script type="text/javascript" src="<?=url('cms/templates/admin/js') . '/' . $js ?>"></script>
         <?php endforeach; ?>
+        <script type="text/javascript">
+            $(function(){
+
+                $('#nav li').has('ul').addClass('parent');
+
+                var windiwVidth = $(window).outerWidth();
+                if(windiwVidth <= 800){
+                    $('#toggleMenu').on('click', function(){
+                        $('#nav li').removeClass('hover');
+                        $('#nav').toggle();
+                        return false;
+                    })
+
+                    $('#nav a').on('click', function(){
+                        var el = $(this).parent('li');
+                        $('#nav li').not(el).not(el.parents()).removeClass('hover');
+                        el.toggleClass('hover');
+
+                        if(el.hasClass('parent'))
+                            return false;
+                    });
+
+                } else {
+
+                    $('#nav a').on('click', function(){
+                        if($(this).parent('li').hasClass('parent'))
+                            return false;
+                    });
+
+                    $('#nav li').hover(function(){
+                        $(this).addClass('hover');
+                    }, function(){
+                        $(this).removeClass('hover');
+                    });
+                }
+            });
+        </script>
     </head>
     <body>
         <section id="wrapper">
@@ -16,13 +54,38 @@
                 <div class="header-bg"></div>
                 <nav id="header-nav">
                     <div class="container">
-                        <ul>
-                            <li class="parent-active"><a href="#">Администрирование</a></li>
-                            <li><a href="#">Статистика</a></li>
-                            <li><a href="#">Статистика</a></li>
-                            <li><a href="#">Статистика</a></li>
+                        <a href="#" id="toggleMenu">
+                            <div class="toggleMenuItem"></div>
+                        </a>
+                        <ul id="nav">
+                            <li>
+                                <a href="#">Администрирование</a>
+                                <ul>
+                                    <li><a href="#">подпункт 1</a></li>
+                                    <li><a href="#">подпункт 2</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Пункт 2</a></li>
+                            <li>
+                                <a href="#">Пункт 3</a>
+                                <ul>
+                                    <li><a href="#">подпункт 1</a></li>
+                                    <li>
+                                        <a href="#">подпункт 2</a>
+                                        <ul>
+                                            <li><a href="#">aaa</a></li>
+                                            <li>
+                                                <a href="#">bbb</a>
+                                                <ul>
+                                                    <li class="hover"><a href="#">под подпункт 1 подпункт 1</a></li>
+                                                    <li><a href="#">под подпункт 2</a></li>
+                                                </ul>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </li>
                         </ul>
-                        <div class="clear"></div>
                     </div>
                 </nav>
             </header>
